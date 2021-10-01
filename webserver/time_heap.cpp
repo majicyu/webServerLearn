d7721e39750d5d15a6a48d321d31e3d4124d20d0
majicyu <qaz13397035986@gmail.com>
2021-10-01 23:30:36 +0800

    time_heap.cpp

--- webserver/time_heap.cpp
+++ webserver/time_heap.cpp
@@ -76,5 +76,104 @@ public:
         if(cur_size >= capacity){
             resize();
         }
+
+        int hole = cur_size++;
+        int parent = 0;
+
+        for(;hole>0;hole=parent){
+            parent = (hole-1)/2;
+            if(array[parent]->expire <= timer->expire){
+                break;
+            }
+            array[hole] = array[parent];
+        }
+        array[hole] = timer;
     }
-}
+
+    void del_timer(heap_timer* timer){
+        if(!timer){
+            return;
+        }
+        timer->cb_func = NULL;
+    }
+
+    heap_timer* top() const{
+        if(empty()){
+            return NULL;
+        }
+        return array[0];
+    }
+
+    void pop_timer(){
+        if(empty()){
+            return;
+        }
+        if(array[0]){
+            delete array[0];
+            array[0] = array[--cur_size];
+            percolate_down(0);
+        }
+    }
+
+    void tick(){
+        heap_timer* tmp = array[0];
+        time_t cur = time(NULL);
+        while(!empty()){
+            if(!tmp){
+                break;
+            }
+            if(tmp->expire > cur){
+                break;
+            }
+            if(array[0]->cb_func){
+                array[0]->cb_func(array[0]->user_data);
+            }
+            pop_timer();
+            tmp = array[0];
+        }
+    }
+    bool empty() const {return cur_size == 0;}
+
+private:
+    void percolate_down(int hole){
+        heap_timer* temp = array[hole];
+        int child = 0;
+        for(;((hole*2+1) <= (cur_size-1));hole=child){
+            child = hole*2+1;
+            if((child < (cur_size-1)) && (array[child+1]->expire < array[child]->expire)){
+                child++;
+            }
+            if(array[child]->expire < temp->expire){
+                array[hole] = array[child];
+            }
+            else{
+                break;
+            }
+        }
+        array[hole] = temp;
+    }
+
+    void resize() throw (std::exception){
+        heap_timer** temp = new heap_timer* [2*capacity];
+        for(;int i=0;i<2*capacity;++i){
+            temp[i] =NULL;
+        }
+        if(!temp){
+            throw std::exception();
+        }
+        capacity = 2*capacity;
+        for(int i=0;i<cur_size;++i){
+            temp[i] = array[i];
+        }
+        delete [] array;
+        array = temp;
+    }
+
+private:
+    heap_timer** array;
+    int capacity;
+    int cur_size;
+    
+};
+
+#endif
